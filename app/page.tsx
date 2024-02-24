b9ef02e1a99454874b3ec8c10d961fe864284f1e
jai prashanth <Jaiprashanth003@gmail.com>
2024-02-24 19:10:00 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <header> <Navbar/></header>
+      <Hero/>
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <GetApp/>
+    </div>
+  );
+}
